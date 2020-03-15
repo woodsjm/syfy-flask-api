9f84ad6e567a4a6e04a398cec9d7edbc1d884b65
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-03-15 04:29:59 -0500

    Can now store fetched images in db

--- processes/background.py
+++ processes/background.py
@@ -1,14 +1,23 @@
 import requests
 
-
 from credentials.auth import name, key, secret
 from utils.image_api import ImageApi
 
 
 image_api = ImageApi(name, key, secret)
 
-def update_image_data(db):
+def update_image_data(mongo):
     code, reason, images = image_api.fetch_image_data()
     if code == 200:
-        print(images)
-    return "working"
+        try:
+            db = mongo.db
+            coll = db['images']
+            store_images = coll.insert(images)
+        except:
+            return "Failed"
+        else:
+            docs = []
+            for doc in store_images:
+                docs.append(doc)
+            return "Success"
+        
