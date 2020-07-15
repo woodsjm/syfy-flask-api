440d6bc9bcbb9b04109a58dd5458f93600f9a326
Jordan Woods <jordan.woods@nlaw.northwestern.edu>
2020-07-14 22:35:26 -0500

    Debugging ip handler

--- utils/ip_handler.py
+++ utils/ip_handler.py
@@ -8,7 +8,7 @@ def handle_ip(remote_ip):
     api_key = os.environ.get('IPTOEARTH_API_KEY')
     client_id = os.environ.get('IPTOEARTH_CLIENT_ID')
     request_response = request.Request('https://iptoearth.expeditedaddons.com/?api_key=' + api_key + '&ip=' + remote_ip)
-    result = urlopen(request_response).read()
+    result = request.urlopen(request_response).read()
     print("RESULT: ", result)
     return
 
